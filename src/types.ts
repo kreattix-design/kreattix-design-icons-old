020a598f1853bdad770cfc00fbacb7ab30f39d88
Prince Patel <ext_prpatel@falabella.cl>
2022-11-03 11:39:32 +0530

    name list props

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
 export type IconNames = 'Logo' | 'Close' | 'Plus'
+export interface IconNameListProps {
+  [key: string]:
+    | string
+    | React.FunctionComponent<{}>
+    | React.ComponentClass<{}, unknown>
+}
